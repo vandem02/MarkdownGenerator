de40b58755c56faf0881f0f8eb719298bdc3afdc
Evan Vandenberg <vandem02@uwgb.edu>
2023-07-30 17:38:46 -0500

    Added title and description prompts, added dist folder for created README

---
+++ dist/README.md
@@ -0,0 +1,27 @@
+# Bleh 
+![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+## Description
+Pizza
+## Deployed Application URL
+h
+## Screenshot
+![alt-text](h)
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Features
+It can poop
+## Languages & Dependencies
+node, npm
+## How to Use This Application:
+Java
+## Contributors
+
+## Testing
+Test1
+## Questions
+Please send your questions [here](mailto:evanmavnden@gmail.com?subject=[GitHub]%20Dev%20Connect) or visit [github/vandem02](https://github.com/vandem02).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return license !== "none" ? `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)` : "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +14,36 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title} 
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text](${data.screenshot})
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Features
+${data.features}
+## Languages & Dependencies
+${data.require}
+## How to Use This Application:
+${data.usage}
+## Contributors
+${data.contributors}
+## Testing
+${data.test}
+## Questions
+Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
 `;
 }
+
+module.exports = generateMarkdown;
 
 module.exports = generateMarkdown;
